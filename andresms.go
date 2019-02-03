16ea0f7f22008f5f92cf2f27952d5e22b7c0abfe
Andres Muquinche <andresmuquinche@gmail.com>
2019-02-03 03:51:15 -0500

    Se ajusta la URL de la opción 2 para ordenar por nombre, se agregan comentarios y se cambia el uso de fmt pot bufio

--- andresms.go
+++ andresms.go
@@ -14,20 +14,20 @@ import (
 	"time"
 )
 
-//APIresponse primer nivel
+//APIresponse estructura primer nivel
 type APIresponse struct {
 	Data Data `json:"data"`
 }
 
-//Data segundo nivel
+//Data estructura segundo nivel
 type Data struct {
 	Results []Results `json:"results"`
 }
 
-//Results segundo nivel
+//Results estructura segundo nivel
 type Results struct {
 	ID          int       `json:"id"`
-	Nombre      string    `json:"name"` //llenar todos los datos
+	Nombre      string    `json:"name"`
 	Descripcion string    `json:"description"`
 	Modificado  string    `json:"modified"`
 	Comics      Comics    `json:"comics"`
@@ -36,88 +36,91 @@ type Results struct {
 	Eventos     Eventos   `json:"events"`
 }
 
-//Comics tercer nivel
+//Comics estructura tercer nivel
 type Comics struct {
 	Disponible int     `json:"available"`
 	Items      []Items `json:"items"`
 }
 
-//Series tercer nivel
+//Series estructura tercer nivel
 type Series struct {
 	Disponible int     `json:"available"`
 	Items      []Items `json:"items"`
 }
 
-//Historias tercer nivel
+//Historias estructura tercer nivel
 type Historias struct {
 	Disponible int     `json:"available"`
 	Items      []Items `json:"items"`
 }
 
-//Eventos tercer nivel
+//Eventos estructura tercer nivel
 type Eventos struct {
 	Disponible int     `json:"available"`
 	Items      []Items `json:"items"`
 }
 
-//Items cuarto nivel
+//Items estructura cuarto nivel
 type Items struct {
 	Nombre string `json:"name"`
 }
 
 func main() {
 
-	ts := time.Now().Format("20060102150405") //"20060102150405" constantes de Go para el formato del timestamp
-	publicKey, privateKey := "4de5c74cfbb660b526a8ce118904822d", "7dc5edeb782cdb834b0c66a54b68dff0eb6add8e"
+	ts := time.Now().Format("20060102150405")                                                               //se obtiene el timestamp y se formatea
+	publicKey, privateKey := "4de5c74cfbb660b526a8ce118904822d", "7dc5edeb782cdb834b0c66a54b68dff0eb6add8e" //llaves publica y privada
 	hash := md5.New()
-	io.WriteString(hash, ts+privateKey+publicKey)
-	hashhex := hex.EncodeToString(hash.Sum(nil))
+	io.WriteString(hash, ts+privateKey+publicKey) //se obtiene el md5
+	hashhex := hex.EncodeToString(hash.Sum(nil))  //Formato hexadecimal para usar en la URL
 
 	fmt.Println("Este programa consume datos de la API de Marvel")
 	fmt.Println("Seleccione la opción deseada:\n1. Busqueda\n2. Listar")
-	var input uint8
-	fmt.Scanf("%o\n", &input)
 
-	switch input {
-	case 1:
+	reader := bufio.NewReader(os.Stdin) //se usa bufio en lugar de fmt por existir en error en el momento de iniciar
+	char, _, err := reader.ReadRune()
+	if err != nil {
+		fmt.Println(err)
+	}
+	switch char {
+	case 49: //ascii para 1 y 2
 		fmt.Println("¿Que personaje desea encontrar?:")
 		scanner := bufio.NewScanner(os.Stdin)
-		scanner.Scan()
+		scanner.Scan() //se recibe el nombre a buscar
 		text := scanner.Text()
-		nombre := url.QueryEscape(text)
-		url := fmt.Sprintf("http://gateway.marvel.com/v1/public/characters?name=%s&ts=%s&apikey=%s&hash=%s", nombre, ts, publicKey, hashhex)
-		contents, err := GetRequest(url)
+		nombre := url.QueryEscape(text)                                                                                                      //si existe un espacio se da formato para evitar error en la solicitud
+		url := fmt.Sprintf("http://gateway.marvel.com/v1/public/characters?name=%s&ts=%s&apikey=%s&hash=%s", nombre, ts, publicKey, hashhex) //URL lista para enviar la solicitud
+		contents, err := GetRequest(url)                                                                                                     // se envia la solicitud
 		if err != nil {
 			fmt.Println(err)
 		}
 		var s APIresponse
-		err = json.Unmarshal(contents, &s)
+		err = json.Unmarshal(contents, &s) //recibe los datos y almacena en el formato de la estructura
 		if err != nil {
 			fmt.Println(err)
 		}
-		b, err := json.MarshalIndent(s.Data.Results, "", "  ")
+		b, err := json.MarshalIndent(s.Data.Results, "", "  ") //Se agrega indentacion y mejor presentacion a los datos
 		if err != nil {
 			fmt.Println(err)
 		}
-		os.Stdout.Write(b)
+		os.Stdout.Write(b) //se imprime
 
-	case 2:
+	case 50: //ascii para 2
 
-		url := fmt.Sprintf("http://gateway.marvel.com/v1/public/characters?ts=%s&apikey=%s&hash=%s", ts, publicKey, hashhex)
-		contents, err := GetRequest(url)
+		url := fmt.Sprintf("http://gateway.marvel.com/v1/public/characters?orderBy=name&ts=%s&apikey=%s&hash=%s", ts, publicKey, hashhex) //url para la solicitud
+		contents, err := GetRequest(url)                                                                                                  //envia la solicitud
 		if err != nil {
 			fmt.Println(err)
 		}
 		var s APIresponse
-		err = json.Unmarshal(contents, &s)
+		err = json.Unmarshal(contents, &s) //recibe los datos y almacena en el formato de la estructura
 		if err != nil {
 			fmt.Println(err)
 		}
-		b, err := json.MarshalIndent(s.Data.Results, "", "  ")
+		b, err := json.MarshalIndent(s.Data.Results, "", "  ") //Se agrega indentacion y mejor presentacion a los datos
 		if err != nil {
 			fmt.Println(err)
 		}
-		os.Stdout.Write(b)
+		os.Stdout.Write(b) //se imprime
 
 	default:
 		fmt.Println("La opción seleccionada no es válida")
@@ -127,16 +130,16 @@ func main() {
 
 //GetRequest peticion
 func GetRequest(url string) ([]byte, error) {
-	resp, err := http.Get(string(url))
+	resp, err := http.Get(string(url)) //se envia la solicitud con la URL recibida
 	if err != nil {
 		fmt.Println(err)
 	}
-	defer resp.Body.Close()
-	contents, err := ioutil.ReadAll(resp.Body)
+	defer resp.Body.Close()                    //Se cierra la conexion
+	contents, err := ioutil.ReadAll(resp.Body) //lee los datos
 	if err != nil {
 		fmt.Println(err)
 	}
 	fmt.Print("Solicitando...")
-	return []byte(contents), err
+	return []byte(contents), err //devuelve el contenido o el error
 
 }
